c3e7d58381694c18d75a67ba9091f6a373d3e9a1
Sharan Suri <sharansuri-8@hotmail.com>
2026-01-09 01:30:35 +0530

    Disabled Webengage

--- webengage.js
+++ webengage.js
@@ -1,57 +1,35 @@
-//WebEngage -- Start
-const webengageScript = document.createElement("script");
-webengageScript.id = "_webengage_script_tag";
-webengageScript.type = "text/javascript";
-webengageScript.textContent = `
-var webengage;
-!(function (w, e, b, n, g) {
-  function o(e, t) {
-    e[t[t.length - 1]] = function () {
-      r.__queue.push([t.join("."), arguments]);
-    };
-  }
-  var i,
-    s,
-    r = w[b],
-    z = " ",
-    l = "init options track screen onReady".split(z),
-    a =
-      "webPersonalization feedback survey notification notificationInbox".split(
-        z
-      ),
-    c = "options render clear abort".split(z),
-    p = "Prepare Render Open Close Submit Complete View Click".split(z),
-    u = "identify login logout setAttribute".split(z);
-  if (!r || !r.__v) {
-    for (
-      w[b] = r = { __queue: [], __v: "6.0", user: {} }, i = 0;
-      i < l.length;
-      i++
-    )
-      o(r, [l[i]]);
-    for (i = 0; i < a.length; i++) {
-      for (r[a[i]] = {}, s = 0; s < c.length; s++) o(r[a[i]], [a[i], c[s]]);
-      for (s = 0; s < p.length; s++) o(r[a[i]], [a[i], "on" + p[s]]);
-    }
-    for (i = 0; i < u.length; i++) o(r.user, ["user", u[i]]);
-    setTimeout(function () {
-      var f = e.createElement("script"),
-        d = e.getElementById("_webengage_script_tag");
-      (f.type = "text/javascript"),
-        (f.async = !0),
-        (f.src =
-          ("https:" == e.location.protocol
-            ? "https://widgets.in.webengage.com"
-            : "http://widgets.in.webengage.com") +
-          "/js/webengage-min-v-6.0.js"),
-        d.parentNode.insertBefore(f, d);
-    });
-  }
-})(window, document, "webengage");
-webengage.init("in~aa131665");
-`;
-document.head.appendChild(webengageScript);
-//WebEngage -- End
+//WebEngage -- Mock Implementation (No Data Sent)
+// Create a dynamic mock WebEngage object using Proxies
+(function () {
+  // Create a recursive proxy that handles any property/method access
+  const createMockProxy = () => {
+    return new Proxy(
+      {},
+      {
+        get: function (target, prop) {
+          // Return specific values for known properties
+          if (prop === "__v") return "6.0";
+          if (prop === "__queue") return [];
+
+          // For any other property access, return a function that:
+          // 1. Does nothing (no-op)
+          // 2. Returns the proxy itself for chaining
+          return function () {
+            return createMockProxy();
+          };
+        },
+      }
+    );
+  };
+
+  // Set the mock webengage object
+  window.webengage = createMockProxy();
+
+  console.log(
+    "[WebVeda] Mock WebEngage initialized - no data will be sent to WebEngage servers"
+  );
+})();
+//WebEngage -- Mock Implementation End
 
 window.course_data = [
   {
